5c5a61f68207aa1aa967bc52cce9b3ec2514d7ba
Rafael Odzakow <rafael.odzakow@trioptima.com>
2022-10-31 21:52:54 +0100

    asd

--- run.py
+++ run.py
@@ -4,37 +4,43 @@ from __future__ import division
 import RPi.GPIO as GPIO
 import time
 import os
+import shutil
 
 GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
 PIN = 21
 GPIO.setup(PIN, GPIO.OUT) # GPIO Assign mode
 GPIO.setwarnings(False)
 
+os.environ["PS1"] = "\e[?1;2;32;c"
+columns = shutil.get_terminal_size().columns
 
-def set_color(color):
+def bgcolor(color):
     os.system("/home/casanova/casanova/colors.sh " + color)
+
+def txt(text):
+    print("\n\n\n" + text.center(columns))
 
 def input_password():
     try:
         while True:
-            set_color("black")
+            bgcolor("black")
             x = input("")
             if x == "casanova!" or x == "c":
-                set_color("green")
-                print("correct password")
+                bgcolor("green")
+                txt("correct password")
                 trigger_relay()
             if x == "exit123" or x == "e":
-                set_color("black")
-                print("exiting!")
+                bgcolor("black")
+                txt("exiting!")
                 GPIO.cleanup()
                 exit(0)
             else:
-                set_color("red")
-                print("wrong password, try again")
+                bgcolor("red")
+                txt("wrong password, try again")
                 time.sleep(3)
     except KeyboardInterrupt:
-        set_color("black")
-        print("wrong password, try again")
+        bgcolor("black")
+        txt("wrong password, try again")
         input_password()
 
 
@@ -42,10 +48,10 @@ def trigger_relay():
     try:
         while True:
             GPIO.output(PIN, GPIO.HIGH)
-            #print('pin21 on')
+            #txt('pin21 on')
             time.sleep(1)
             GPIO.output(PIN, GPIO.LOW)
-            #print('pin21 off')
+            #txt('pin21 off')
             time.sleep(1)
     except KeyboardInterrupt:
         input_password()
